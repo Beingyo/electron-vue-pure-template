7416f4f17d63e91008825399563bdb67009ffc0d
Beingyo <413009063@qq.com>
2021-08-09 14:15:41 +0800

    2021-08-09

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,8 @@ const { build, version } = require('../../package.json')
 
 const config = {
   version: version, // 版本号
-  appName: build.productName, // 鼠标悬停显示
+  // appName: build.productName, // 鼠标悬停显示
+  appName: '托盘名称', // 鼠标悬停显示
   isUseTray: true, // 使用托盘
   isClose: true, // 关闭事件
   isAutoUpdate: true, // 自动更新
